82210613ef5975f6a1dae28f2e1f6dfb2672de46
Rudra1125 <shankarrudra36@gmail.com>
2022-03-22 23:23:20 +0530

    Created sign in sign out

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,11 @@ router.get('/signin',usersController.signin );
 router.get('/signup',usersController.signup);
 
 router.post('/create', usersController.create);
- 
+router.post('/create-session', usersController.createSession);
+router.post('/signout',usersController.logout);
+// {
+//     res.clearCookie('user_id');
+//     // req.createSession.destroy();
+//     res.redirect('/users/signin');
+// })
 module.exports=  router;
