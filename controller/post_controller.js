9ea0f3708c79f48b62214eafe4aada13a5780ba7
Rudra1125 <shankarrudra36@gmail.com>
2022-09-29 17:46:56 +0530

    delete post

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const Comment = require('../models/comment');
 module.exports.create = function(req,res){
     Post.create({
         content: req.body.content,
@@ -9,5 +9,26 @@ module.exports.create = function(req,res){
 
         return res.redirect('back');
     })
+}
+
+module.exports.destroy = function (req,res) {
+    const id = req.params.id.trim()
+    console.log(id)
+    Post.findById(id, function (err, post) {
+        if (err) { console.log(err, "Error in fething the post"); return; }
+        // .id means converting the object id into string
+        const idd=req.user.id.trim()
+        // here we have to do == to compare the strings
+        
+        if (post.user == idd) {
+            
+            post.remove();
+            Comment.deleteMany({ post: id }, function (err) {
+                return res.redirect('back');
+            }) 
+        } else {
+            return res.redirect('back');
+        }
+    });
 }
 
