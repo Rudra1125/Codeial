e5f881ee986ac06d4f28d328f515db2570fd6ee7
Rudra1125 <shankarrudra36@gmail.com>
2022-09-30 23:24:59 +0530

    creating post through ajax

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -3,17 +3,26 @@ const Comment = require('../models/comment');
 module.exports.create = async function(req,res){
     
     try {
-        let post=await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
-        })
+        });
+
+        if (req.xhr) {
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message: "Post-Created!"
+            })
+        }
         
         req.flash('success','Post Published')
         return res.redirect('back');
         
     } catch (err) {
         req.flash('error','err')
-        console.log("error", error)
+        console.log("error", err)
     }
     
 }
